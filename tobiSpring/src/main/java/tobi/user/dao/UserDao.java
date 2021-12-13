344393863fa5156d45fd1cf0c8ce028d3ae41153
Youngjin <dudwls0505@naver.com>
2021-12-13 19:01:25 +0900

    UserDaoTest클래스로 클라이언트코드 분리

--- tobiSpring/src/main/java/tobi/user/dao/UserDao.java
+++ tobiSpring/src/main/java/tobi/user/dao/UserDao.java
@@ -9,12 +9,10 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker; // 인터페이스이므로 구체적인 클래스정보를 알필요가없다.
 
-    public UserDao() {
-        connectionMaker = new DConnectionMaker(); // 구체적인 클래스에 의존... 문제발생 또다시 원점이다.
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker; // 구체적인 클래스에 의존... 문제발생 또다시 원점이다.
 
     }
-
-
 
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection c = connectionMaker.makeConnection();
@@ -28,7 +26,6 @@ public class UserDao {
         c.close();
 
     }
-
 
     public User get(String id) throws ClassNotFoundException, SQLException {
         Connection c = connectionMaker.makeConnection();
@@ -47,26 +44,6 @@ public class UserDao {
         c.close();
 
         return user;
-    }
-
-
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new UserDao();
-        User user = new User();
-        user.setId("LeeYoungJin");
-        user.setName("이영진");
-        user.setPassword("1234");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + "등록 성공");
-
-        User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + "조회 성공");
-
-
     }
 
 
