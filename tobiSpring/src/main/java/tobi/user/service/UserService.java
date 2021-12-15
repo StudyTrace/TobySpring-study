0d0cc7278d544fe35f828281411142aec856b75e
Youngjin <dudwls0505@naver.com>
2021-12-16 00:57:56 +0900

    스프링이 제공하는 트랜잭션 추상화

--- tobiSpring/src/main/java/tobi/user/service/UserService.java
+++ tobiSpring/src/main/java/tobi/user/service/UserService.java
@@ -1,29 +1,28 @@
 package tobi.user.service;
 
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import tobi.user.dao.UserDao;
 import tobi.user.domain.Level;
 import tobi.user.domain.User;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
     UserDao userDao;
 
-    private DataSource dataSource;
 
+    private PlatformTransactionManager transactionManager;
 
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECCOMEND_FOR_GOLD = 30;
 
-
-
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
     }
 
     public void setUserDao(UserDao userDao) {
@@ -33,9 +32,18 @@ public class UserService {
 
     public void upgradeLevels() throws SQLException {
 
-        TransactionSynchronizationManager.initSynchronization(); // 트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화
-        Connection c = DataSourceUtils.getConnection(dataSource); // DB커넥션을 생성하고 트랜잭션 시작 , 이후의 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
-        c.setAutoCommit(false);
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource); // JDBC트랜잭션 추상오브젝트 생성
+
+        /**
+         * PlatformTransactionManager(스프링이 제공하는 트랜잭션 경계설정을 위한 추상인터페이스 )로 시작한 트랜잭션은 트랜낵션 동기화 저장소에 저장된다.
+         * PlatformTransactionManager를 구현한 DatasourceTransactionManager 오브젝트는 JdbcTemplate에서 사용될수잇는 방식으로 트랜잭션을 관리해준다.
+         */
+//
+//        TransactionSynchronizationManager.initSynchronization(); // 트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화
+//        Connection c = DataSourceUtils.getConnection(dataSource); // DB커넥션을 생성하고 트랜잭션 시작 , 이후의 작업은 모두 여기서 시작한 트랜잭션 안에서 진행된다.
+//        c.setAutoCommit(false);
+
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
             List<User> users = userDao.getAll();
@@ -44,16 +52,17 @@ public class UserService {
                     upgradeLevel(user);
                 }
             }
-            c.commit();
+            transactionManager.commit(status);
         } catch (Exception e) {
-            c.rollback(); // 예외발생시 롤백
+            transactionManager.rollback(status); // 예외발생시 롤백
             throw e;
-
-        }finally {
-            DataSourceUtils.releaseConnection(c, dataSource); // 안전하게 DB커넥션을 닫음
-            TransactionSynchronizationManager.unbindResource(this.dataSource);
-            TransactionSynchronizationManager.clearSynchronization();  // 동기화 작업 종료 및 정리
         }
+
+//        }finally {
+//            DataSourceUtils.releaseConnection(c, dataSource); // 안전하게 DB커넥션을 닫음
+//            TransactionSynchronizationManager.unbindResource(this.dataSource);
+//            TransactionSynchronizationManager.clearSynchronization();  // 동기화 작업 종료 및 정리
+//        }
 
     }
 
--- tobiSpring/src/main/java/tobi/whiteship/DemoApp.java
+++
@@ -1,4 +0,0 @@
-package tobi.whiteship;
-
-public class DemoApp {
-}
