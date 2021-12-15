a1d4d7b8385e17032226f5963a790ae560e10242
Youngjin <dudwls0505@naver.com>
2021-12-15 20:45:58 +0900

    level,login,recommend User필드 추가

--- tobiSpring/src/test/java/tobi/user/dao/UserDaoJdbcTest.java
+++ tobiSpring/src/test/java/tobi/user/dao/UserDaoJdbcTest.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobi.user.domain.Level;
 import tobi.user.domain.User;
 
 import java.util.List;
@@ -42,9 +43,9 @@ class UserDaoJdbcTest {
     void setUp() {
         this.dao = context.getBean("userDao", UserDaoJdbc.class);
         System.out.println("before 실행");
-        this.user1 = new User("LeeYoungJin", "이영진", "1234");
-        this.user2 = new User("KimYoungJin", "김영진", "5678");
-        this.user3 = new User("ParkYoungJin", "박영진", "9012");
+        this.user1 = new User("LeeYoungJin", "이영진", "1234", Level.BASIC, 1, 0);
+        this.user2 = new User("KimYoungJin", "김영진", "5678", Level.SILVER, 55, 10);
+        this.user3 = new User("ParkYoungJin", "박영진", "9012", Level.GOLD, 100, 40);
         System.out.println(this.context);
         System.out.println(this);
         System.out.println("아이디뽑기" + this.user1.getId() + this.user2.getId() + this.user3.getId());
@@ -61,13 +62,11 @@ class UserDaoJdbcTest {
         assertThat(dao.getCount(), is(2));
 
         User userget1 = dao.get(user1.getId());
-        assertThat(user1.getName(), is(user1.getName()));
-        assertThat(user1.getPassword(), is(user1.getPassword()));
+        checkSumUser(userget1, user1);
 
 
         User userget2 = dao.get(user2.getId());
-        assertThat(user2.getName(), is(user2.getName()));
-        assertThat(user2.getPassword(), is(user2.getPassword()));
+        checkSumUser(userget2, user2);
 
     }
 
@@ -143,6 +142,9 @@ class UserDaoJdbcTest {
         assertThat(user1.getId(),is(user2.getId()));
         assertThat(user1.getName(),is(user2.getName()));
         assertThat(user1.getPassword(),is(user2.getPassword()));
+        assertThat(user1.getLevel(), is(user2.getLevel()));
+        assertThat(user1.getLogin(), is(user2.getLogin()));
+        assertThat(user1.getRecommend(), is(user2.getRecommend()));
     }
 
 
